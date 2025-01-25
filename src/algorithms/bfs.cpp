d0170f958da6710869089775337a93c7b9e31300
CyberLord <112614934+CyberLoord@users.noreply.github.com>
2025-01-26 00:24:03 +0300

    Update bfs.cpp

---
+++ src/algorithms/bfs.cpp
@@ -0,0 +1,27 @@
+#include "graphAlgorithms.h"
+#include <queue>
+#include <vector>
+#include <iostream>
+
+std::vector<int> GraphAlgorithms::bfs(int startVertex) {
+    const int numVertices = graph.getNumVertices();
+    std::vector<int> distances(numVertices, -1);
+    std::queue<int> q;
+
+    distances[startVertex] = 0;
+    q.push(startVertex);
+
+    while (!q.empty()) {
+        int vertex = q.front();
+        q.pop();
+
+        for (int i = 0; i < numVertices; ++i) {
+            if (graph.getAdjMatrix()[vertex][i] != 0 && distances[i] == -1) {
+                distances[i] = distances[vertex] + 1;
+                q.push(i);
+            }
+        }
+    }
+
+    return distances;
+}
