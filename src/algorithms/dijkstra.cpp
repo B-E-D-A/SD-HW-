b6328cf5f81c811aadb3b6349f3258cb866284d1
CyberLord <112614934+CyberLoord@users.noreply.github.com>
2025-02-12 21:01:11 +0300

    Update dijkstra.cpp

--- src/algorithms/dijkstra.cpp
+++ src/algorithms/dijkstra.cpp
@@ -1,10 +1,34 @@
 #include "graphAlgorithms.h"
-#include <iostream>
+#include <vector>
+#include <queue>
+#include <limits>
 
 std::vector<int> GraphAlgorithms::dijkstra(int startVertex) {
-    std::cout << "Dijkstra's algorithm called with startVertex: " << startVertex << std::endl;
+    const int numVertices = graph.getNumVertices();
+    std::vector<int> distances(numVertices, std::numeric_limits<int>::max());
+    std::priority_queue<std::pair<int, int>, std::vector<std::pair<int, int>>, std::greater<>> pq;
 
-    // TODO: Реализовать алгоритм Дейкстры
+    distances[startVertex] = 0;
+    pq.push({0, startVertex});
 
-    return {};
+    while (!pq.empty()) {
+        int dist = pq.top().first;
+        int vertex = pq.top().second;
+        pq.pop();
+
+        if (dist > distances[vertex]) continue;
+
+        for (int i = 0; i < numVertices; ++i) {
+            int weight = graph.getAdjMatrix()[vertex][i];
+            if (weight > 0) {  // Учитываем только существующие рёбра
+                int newDist = distances[vertex] + weight;
+                if (newDist < distances[i]) {
+                    distances[i] = newDist;
+                    pq.push({newDist, i});
+                }
+            }
+        }
+    }
+
+    return distances;
 }
