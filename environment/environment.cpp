4f8b5ee9caba6865ff043090ad46aeca64eac097
Ekaterina Badaeva <badaeva.kate10@gmail.com>
2025-02-28 01:58:15 +0300

    Update environment.cpp

--- environment/environment.cpp
+++ environment/environment.cpp
@@ -9,6 +9,8 @@ static std::unique_ptr<Graph> completeGraph;
 static std::unique_ptr<Graph> randomGraph1;
 static std::unique_ptr<Graph> randomGraph2;
 static std::unique_ptr<Graph> randomGraph3;
+static std::unique_ptr<Graph> treeGraph;
+static std::unique_ptr<Graph> cycleGraph;
 
 Graph createCompleteGraph(int numVertices) {
     std::vector<std::vector<int>> adjMatrix(numVertices, std::vector<int>(numVertices, 1));
@@ -33,147 +35,77 @@ Graph createRandomGraph(int numVertices, int numEdges) {
     return Graph(adjMatrix);
 }
 
+Graph createTreeGraph(int numVertices) {
+    std::vector<std::vector<int>> adjMatrix(numVertices, std::vector<int>(numVertices, 0));
+    for (int i = 1; i < numVertices; ++i) {
+        adjMatrix[i][i - 1] = adjMatrix[i - 1][i] = 1;
+    }
+    return Graph(adjMatrix);
+}
+
+Graph createCycleGraph(int numVertices) {
+    std::vector<std::vector<int>> adjMatrix(numVertices, std::vector<int>(numVertices, 0));
+    for (int i = 0; i < numVertices - 1; ++i) {
+        adjMatrix[i][i + 1] = adjMatrix[i + 1][i] = 1;
+    }
+    adjMatrix[numVertices - 1][0] = adjMatrix[0][numVertices - 1] = 1;
+    return Graph(adjMatrix);
+}
+
 static void SetupGraphs(int numVertices) {
     completeGraph = std::make_unique<Graph>(createCompleteGraph(numVertices));
     randomGraph1 = std::make_unique<Graph>(createRandomGraph(numVertices, numVertices * 2));
     randomGraph2 = std::make_unique<Graph>(createRandomGraph(numVertices, numVertices * 2));
     randomGraph3 = std::make_unique<Graph>(createRandomGraph(numVertices, numVertices * 2));
+    treeGraph = std::make_unique<Graph>(createTreeGraph(numVertices));
+    cycleGraph = std::make_unique<Graph>(createCycleGraph(numVertices));
 }
 
-static void BM_BFS_Complete(benchmark::State& state) {
-    GraphAlgorithms ga(*completeGraph);
-    for (auto _ : state) {
-        ga.bfs(0);
-    }
-}
-BENCHMARK(BM_BFS_Complete);
+#define DEFINE_BENCHMARK(alg, graph) \
+static void BM_##alg##_##graph(benchmark::State& state) { \
+    GraphAlgorithms ga(*graph); \
+    for (auto _ : state) { \
+        ga.alg(0); \
+    } \
+} \
+BENCHMARK(BM_##alg##_##graph);
 
-static void BM_Dijkstra_Complete(benchmark::State& state) {
-    GraphAlgorithms ga(*completeGraph);
-    for (auto _ : state) {
-        ga.dijkstra(0);
-    }
-}
-BENCHMARK(BM_Dijkstra_Complete);
+DEFINE_BENCHMARK(bfs, completeGraph)
+DEFINE_BENCHMARK(dijkstra, completeGraph)
+DEFINE_BENCHMARK(bellmanFord, completeGraph)
+DEFINE_BENCHMARK(aStar, completeGraph)
 
-static void BM_BellmanFord_Complete(benchmark::State& state) {
-    GraphAlgorithms ga(*completeGraph);
-    for (auto _ : state) {
-        ga.bellmanFord(0);
-    }
-}
-BENCHMARK(BM_BellmanFord_Complete);
+DEFINE_BENCHMARK(bfs, randomGraph1)
+DEFINE_BENCHMARK(dijkstra, randomGraph1)
+DEFINE_BENCHMARK(bellmanFord, randomGraph1)
+DEFINE_BENCHMARK(aStar, randomGraph1)
 
-static void BM_AStar_Complete(benchmark::State& state) {
-    GraphAlgorithms ga(*completeGraph);
-    for (auto _ : state) {
-        ga.aStar(0);
-    }
-}
-BENCHMARK(BM_AStar_Complete);
+DEFINE_BENCHMARK(bfs, randomGraph2)
+DEFINE_BENCHMARK(dijkstra, randomGraph2)
+DEFINE_BENCHMARK(bellmanFord, randomGraph2)
+DEFINE_BENCHMARK(aStar, randomGraph2)
 
-static void BM_BFS_Random1(benchmark::State& state) {
-    GraphAlgorithms ga(*randomGraph1);
-    for (auto _ : state) {
-        ga.bfs(0);
-    }
-}
-BENCHMARK(BM_BFS_Random1);
+DEFINE_BENCHMARK(bfs, randomGraph3)
+DEFINE_BENCHMARK(dijkstra, randomGraph3)
+DEFINE_BENCHMARK(bellmanFord, randomGraph3)
+DEFINE_BENCHMARK(aStar, randomGraph3)
 
-static void BM_Dijkstra_Random1(benchmark::State& state) {
-    GraphAlgorithms ga(*randomGraph1);
-    for (auto _ : state) {
-        ga.dijkstra(0);
-    }
-}
-BENCHMARK(BM_Dijkstra_Random1);
+DEFINE_BENCHMARK(bfs, treeGraph)
+DEFINE_BENCHMARK(dijkstra, treeGraph)
+DEFINE_BENCHMARK(bellmanFord, treeGraph)
+DEFINE_BENCHMARK(aStar, treeGraph)
 
-static void BM_BellmanFord_Random1(benchmark::State& state) {
-    GraphAlgorithms ga(*randomGraph1);
-    for (auto _ : state) {
-        ga.bellmanFord(0);
-    }
-}
-BENCHMARK(BM_BellmanFord_Random1);
-
-static void BM_AStar_Random1(benchmark::State& state) {
-    GraphAlgorithms ga(*randomGraph1);
-    for (auto _ : state) {
-        ga.aStar(0);
-    }
-}
-BENCHMARK(BM_AStar_Random1);
-
-static void BM_BFS_Random2(benchmark::State& state) {
-    GraphAlgorithms ga(*randomGraph2);
-    for (auto _ : state) {
-        ga.bfs(0);
-    }
-}
-BENCHMARK(BM_BFS_Random2);
-
-static void BM_Dijkstra_Random2(benchmark::State& state) {
-    GraphAlgorithms ga(*randomGraph2);
-    for (auto _ : state) {
-        ga.dijkstra(0);
-    }
-}
-BENCHMARK(BM_Dijkstra_Random2);
-
-static void BM_BellmanFord_Random2(benchmark::State& state) {
-    GraphAlgorithms ga(*randomGraph2);
-    for (auto _ : state) {
-        ga.bellmanFord(0);
-    }
-}
-BENCHMARK(BM_BellmanFord_Random2);
-
-static void BM_AStar_Random2(benchmark::State& state) {
-    GraphAlgorithms ga(*randomGraph2);
-    for (auto _ : state) {
-        ga.aStar(0);
-    }
-}
-BENCHMARK(BM_AStar_Random2);
-
-static void BM_BFS_Random3(benchmark::State& state) {
-    GraphAlgorithms ga(*randomGraph3);
-    for (auto _ : state) {
-        ga.bfs(0);
-    }
-}
-BENCHMARK(BM_BFS_Random3);
-
-static void BM_Dijkstra_Random3(benchmark::State& state) {
-    GraphAlgorithms ga(*randomGraph3);
-    for (auto _ : state) {
-        ga.dijkstra(0);
-    }
-}
-BENCHMARK(BM_Dijkstra_Random3);
-
-static void BM_BellmanFord_Random3(benchmark::State& state) {
-    GraphAlgorithms ga(*randomGraph3);
-    for (auto _ : state) {
-        ga.bellmanFord(0);
-    }
-}
-BENCHMARK(BM_BellmanFord_Random3);
-
-static void BM_AStar_Random3(benchmark::State& state) {
-    GraphAlgorithms ga(*randomGraph3);
-    for (auto _ : state) {
-        ga.aStar(0);
-    }
-}
-BENCHMARK(BM_AStar_Random3);
+DEFINE_BENCHMARK(bfs, cycleGraph)
+DEFINE_BENCHMARK(dijkstra, cycleGraph)
+DEFINE_BENCHMARK(bellmanFord, cycleGraph)
+DEFINE_BENCHMARK(aStar, cycleGraph)
 
 int main(int argc, char** argv) {
-    ::benchmark::Initialize(&argc, argv);
+    benchmark::Initialize(&argc, argv);
 
     int numVertices = 500;
     SetupGraphs(numVertices);
-
-    ::benchmark::RunSpecifiedBenchmarks();
+    benchmark::RunSpecifiedBenchmarks();
+    benchmark::Shutdown();
     return 0;
 }
